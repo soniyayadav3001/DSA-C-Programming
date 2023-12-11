0083e0c919e36151d2c050ea6a75d39270278ffb
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-12-11 19:59:58 +0530

    Create Insertion before the given data in circular linked list.c

---
+++ Insertion before the given data in circular linked list.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+struct node *first, *temp, *ttemp , *p;
+void create_node(int val){
+    first = (struct node *)malloc(sizeof(struct node));
+    first->data = val;
+    first->next = first;
+}
+void add_node(int a){
+    temp = first;
+    while(temp->next!=first){
+        temp = temp->next;
+    }
+    ttemp=(struct node*)malloc(sizeof(struct node));
+    ttemp->data = a;
+    ttemp->next = first;
+    temp->next=ttemp;
+}
+void in_before(int x , int y){
+    temp = first;
+    while(temp->data!=x){
+        ttemp = temp;
+        temp = temp->next;
+    }
+    p=(struct node*)malloc(sizeof(struct node));
+    ttemp->next = p;
+    p->data = y;
+    p->next = temp;
+}
+void display(){
+    temp = first;
+    do{
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }   while (temp != first);
+}
+int main(){
+    create_node(10);
+    add_node(20);
+    add_node(30);
+    add_node(40);
+    add_node(50);
+    in_before(30, 25);
+    display();
+    return 0;
+}
